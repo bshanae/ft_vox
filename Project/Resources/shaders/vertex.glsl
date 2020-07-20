8875f15c8b8609c4a279df43c6322f06be1bf127
belchenkovova <>
2020-07-20 09:26:55 +0300

    Some garbage in vertex shader deleted

--- Project/Resources/shaders/vertex.glsl
+++ Project/Resources/shaders/vertex.glsl
@@ -9,11 +9,6 @@ out vec4        pass_position;
 
 void            main()
 {
-    mat4       dummy = uniform_view;
-    mat4       dummy1 = uniform_projection;
-    mat4       m = mat4(1);
-    m[3][2] = -0.3;
-
     pass_position = uniform_projection * uniform_view * vec4(in_position, 1.0);
     gl_Position = pass_position;
 }
