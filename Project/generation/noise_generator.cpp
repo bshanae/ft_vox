3480d37dd89b01da5222cadbb8a7470a5a71d313
olesgedz <gedzoles@gmail.com>
2020-08-01 02:19:14 +0300

    testing different noises

---
+++ Project/generation/noise_generator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Oles Gedz on 03/08/2020.
+//
+
+#include "noise_generator.h"
---
+++ Project/generation/noise_generator.h
@@ -0,0 +1,15 @@
+//
+// Created by Oles Gedz on 03/08/2020.
+//
+
+#ifndef VOX_NOISE_GENERATOR_H
+#define VOX_NOISE_GENERATOR_H
+
+
+class noise_generator
+{
+
+};
+
+
+#endif //VOX_NOISE_GENERATOR_H
