ef09d4c8cca8ec7dfc575a65835390de01fc5a6e
belchenkovova <v.belchenko@zmeke.com>
2021-02-22 15:52:28 +0300

    Imported skybox in project

--- project/sources/game/world/skybox/skybox_settings.h
+++ project/sources/game/world/skybox/skybox_settings.h
@@ -7,10 +7,10 @@ namespace				game
 
 struct					game::skybox_settings
 {
-	static inline path	source_right = "/Users/belchenkovova/Downloads/skybox/right.jpg";
-	static inline path	source_left = "/Users/belchenkovova/Downloads/skybox/left.jpg";
-	static inline path	source_top = "/Users/belchenkovova/Downloads/skybox/top.jpg";
-	static inline path	source_bottom = "/Users/belchenkovova/Downloads/skybox/bottom.jpg";
-	static inline path	source_front = "/Users/belchenkovova/Downloads/skybox/front.jpg";
-	static inline path	source_back = "/Users/belchenkovova/Downloads/skybox/back.jpg";
+	static inline path	source_right = "project/resources/skybox/default/right.jpg";
+	static inline path	source_left = "project/resources/skybox/default/left.jpg";
+	static inline path	source_top = "project/resources/skybox/default/top.jpg";
+	static inline path	source_bottom = "project/resources/skybox/default/bottom.jpg";
+	static inline path	source_front = "project/resources/skybox/default/front.jpg";
+	static inline path	source_back = "project/resources/skybox/default/back.jpg";
 };
